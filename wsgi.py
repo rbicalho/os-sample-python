c66aa658f123edefd36a6b8db7075a3b9c757599
rbicalho <rbicalho@gmail.com>
2019-03-21 15:52:12 -0300

    Update wsgi.py
    Includes datetime.now()

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import datetime
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World!" + datetime.datetime.now()
 
 if __name__ == "__main__":
     application.run()
